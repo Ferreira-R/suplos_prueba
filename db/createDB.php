60df6a1b06b712cc4e46d82f44e3951dd8a9f6cd
Ferreira-R <rrferreira99@outlook.com>
2022-04-12 09:42:45 -0500

    modificado mensaje de error al crear db

--- db/createDB.php
+++ db/createDB.php
@@ -7,7 +7,7 @@ $createDB = "CREATE DATABASE Intelcost_bienes";
 if (mysqli_query($conn, $createDB)) {
 	echo "<script>console.log('Database created successfully');</script>";
 } else {
-	echo "<script>console.log('Error creating database: $conn->error');</script>: ";
+	echo "<script>console.log('Error creating database');</script>: ";
 }
 
 $conn->close();
